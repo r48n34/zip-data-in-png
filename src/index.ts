34c9092ce7689ca5e9362279ef4f366052946ff5
reemo <r48n34@gmail.com>
2023-02-15 16:46:17 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const png_in_path = path.join(__dirname, "..", "test-data", "460989.png")
 checkIsPng(png_in_path);
 
 const png_in = fs.readFileSync(png_in_path, {flag:'r'});
-console.log(png_in);
+
 
 // const content_in = path.join(__dirname, "..", "test-data", "hello.zip")
 
@@ -20,19 +20,28 @@ utf8Encode.encode("\x89PNG\r\n\x1a\n").forEach( v => png_out.push(v) )
 
 let i = 8;
 while(true){
-    let chunk_len = int_from_bytes( png_in.subarray(i, i + 4) )
+    let chunk_len_raw = png_in.subarray(i, i + 4) 
+    let chunk_len = int_from_bytes( chunk_len_raw )
+
     let chunk_type = png_in.subarray(i + 4, i + 8)
 
     let chunk_body = png_in.subarray(i + 8, i + 8 + chunk_len)
-    let chunk_csum = int_from_bytes( 
-        png_in.subarray(i + 8 + chunk_len, i + 8 + chunk_len + 4)
-    )
+
+    let chunk_csum_raw = png_in.subarray(i + 8 + chunk_len, i + 8 + chunk_len + 4)
+    let chunk_csum = int_from_bytes( chunk_csum_raw )
 
     console.log("chunk_len:", chunk_len)
 	console.log("chunk_type:", chunk_type.toString())
 	console.log("chunk_body:", chunk_body)
     console.log("chunk_csum:", chunk_csum)
 
+    if(["IHDR", "PLTE", "IDAT", "IEND"].indexOf(chunk_type.toString()) === -1){
+        console.log("Warning: dropping non-essential or unknown chunk:", chunk_type.toString())
+		continue
+    }
+
+
+    i += (i + 8 + chunk_len + 4)
     break;
 }
 
