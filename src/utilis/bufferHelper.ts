3d76ffb81b54d70589b3e6da05bf148b50dbcfa6
reemo <r48n34@gmail.com>
2023-02-15 16:54:54 +0800

    Update bufferHelper.ts

--- src/utilis/bufferHelper.ts
+++ src/utilis/bufferHelper.ts
@@ -1,5 +1,5 @@
 // https://stackoverflow.com/questions/54257476/is-there-any-function-in-js-like-this-int-from-bytes-in-python
-export function int_from_bytes(bufferArray: any){
+export function int_from_bytes(bufferArray: Buffer){
     const buf = Buffer.from(bufferArray) // 0x12345678 = 305419896
     return buf.readUInt32BE(0)
 }
