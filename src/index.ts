b583c1f75b9b7a927bc6377dc714740c068e414a
reemo <r48n34@gmail.com>
2023-02-15 16:33:27 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,17 +20,19 @@ utf8Encode.encode("\x89PNG\r\n\x1a\n").forEach( v => png_out.push(v) )
 
 let i = 8;
 while(true){
-    let chunk_len = png_in.subarray(i, i + 4)
+    let chunk_len = int_from_bytes( png_in.subarray(i, i + 4) )
     let chunk_type = png_in.subarray(i + 4, i + 8)
 
-    // let chunk_body = 
+    let chunk_body = png_in.subarray(i + 8, i + 8 + chunk_len)
+    let chunk_csum = int_from_bytes( 
+        png_in.subarray(i + 8 + chunk_len, i + 8 + chunk_len + 4)
+    )
 
-    console.log("chunk_len:", int_from_bytes(chunk_len))
+    console.log("chunk_len:", chunk_len)
 	console.log("chunk_type:", chunk_type.toString())
+	console.log("chunk_body:", chunk_body)
+    console.log("chunk_csum:", chunk_csum)
 
-
-    
-    
     break;
 }
 
