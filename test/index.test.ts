60e251d8e846bda50314df1e6366fd3a9f27c7e9
reemo <r48n34@gmail.com>
2023-02-21 18:17:30 +0800

    Change test case

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,31 @@
 import path from "path";
+import fs from "fs";
+import filetype from 'magic-bytes.js'
+
 import { zipDataInPng } from '../dist/index';
 
 test('zipDataInPng to works normal', () => {
-  expect(
-    zipDataInPng (
-        path.join(__dirname, "..", "test-data", "deno.png"),
-        path.join(__dirname, "..", "test-data", "hello.zip"),
-        path.join(__dirname, "..", "test-data", "helloResultJs.png"),
-    )
-  ).toBe(true);
+
+    let finalFilePath = path.join(__dirname, "..", "test-data", "jestTest.png")
+
+    expect(
+        zipDataInPng (
+            path.join(__dirname, "..", "test-data", "deno.png"),
+            path.join(__dirname, "..", "test-data", "hello.zip"),
+            finalFilePath,
+        )
+    ).toBe(true);
+
+    fs.renameSync(
+        finalFilePath,
+        path.join(__dirname, "..", "test-data", "jestTest.zip")
+    );
+
+    const type = filetype(
+        fs.readFileSync(path.join(__dirname, "..", "test-data", "jestTest.zip"))
+    ).map( v => v.typename );
+
+    expect(type.indexOf("zip") >= 0).toBe(false);
+    fs.unlinkSync(path.join(__dirname, "..", "test-data", "jestTest.zip"));
+
 })
